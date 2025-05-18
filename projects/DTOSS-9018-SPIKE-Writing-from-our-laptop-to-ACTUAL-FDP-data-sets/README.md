82071b55f831f5ff555edf5588951bfe66dec5a5
Graham Jackson <graham.jackson9@nhs.net>
2025-05-18 09:33:03 +0100

    DTOSS-9018: Add upload functionality

--- projects/DTOSS-9018-SPIKE-Writing-from-our-laptop-to-ACTUAL-FDP-data-sets/README.md
+++ projects/DTOSS-9018-SPIKE-Writing-from-our-laptop-to-ACTUAL-FDP-data-sets/README.md
@@ -3,3 +3,8 @@
 The ticket ([DTOSS-9018](https://nhsd-jira.digital.nhs.uk/browse/DTOSS-9018)) has more context and notes.
 
 TODO - add detail
+
+## Troubleshooting
+
+- Apply Schema button not working in Foundry
+  - Make sure the data uploaded is in bytes format.
--- projects/DTOSS-9018-SPIKE-Writing-from-our-laptop-to-ACTUAL-FDP-data-sets/main.py
+++ projects/DTOSS-9018-SPIKE-Writing-from-our-laptop-to-ACTUAL-FDP-data-sets/main.py
@@ -10,29 +10,54 @@ from pprint import pprint
 load_dotenv()
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
 logger = logging.getLogger(__name__)
 
 
-def create_dummy_json_lines_file(n_rows: int = 1_000, filename: str = "dummy", path=Path('/tmp/')) -> str:
+def create_dummy_json_lines_file(filename: str = "dummy", n_rows: int = 1_000, path=Path('/tmp/')) -> str:
 
-    filename = path / f"{filename}--{n_rows}-lines.jsonl"
+    filename_prefix, filename_suffix = filename.split(".")
+    filepath = path / f"{filename_prefix}--{n_rows}-lines.{filename_suffix}"
 
-    with open(filename, "w") as f:
+    with open(filepath, "w") as f:
         for i in range(n_rows + 1):
             f.write(f'{{"id": {i}, "name": "Name {i}"}}\n')
 
-    filesize_mb = os.path.getsize(filename) / (1024 * 1024)
+    filesize_mb = os.path.getsize(filepath) / (1024 * 1024)
 
-    logger.debug(f"Created dummy file with {n_rows:,} JSON lines: {filename}, {filesize_mb:.2f} MB")
+    logger.info(f"Created dummy file with {n_rows:,} JSON lines: {filepath}, {filesize_mb:.2f} MB")
 
-    return filename
+    return str(filepath)
+
+
+def get_foundry_client(host: str, token: str) -> FoundryClient:
+    return foundry_sdk.FoundryClient(
+        auth=foundry_sdk.UserTokenAuth(token),
+        hostname=host,
+    )
+
+
+def upload_file_to_foundry_dataset(client: FoundryClient, dataset_rid: str, filepath_local: str, filename_foundry: str) -> None:
+    with open(filepath_local, "rb") as f:
+        client.datasets.Dataset.File.upload(
+            dataset_rid=dataset_rid,
+            file_path=filename_foundry,
+            body=f.read()
+        )
+    logger.info(f"File {filepath_local} uploaded to foundry as {filename_foundry}.")
 
 
 if __name__ == "__main__":
-    filename = create_dummy_json_lines_file()
+    token: str = os.environ["BEARER_TOKEN"]
+    host: str = os.environ["HOSTNAME"]
+    dataset_rid: str = os.environ["DATASET_RID"]
 
+    filename_local: str = 'dummy.jsonl'
+    filename_foundry: str = 'dummy_fdp.jsonl'
 
+    filepath_local = create_dummy_json_lines_file(filename_local)
+    client = get_foundry_client(host, token)
+    upload_file_to_foundry_dataset(client, dataset_rid, filepath_local, filename_foundry)
