035844d9dca86d984835ce8e4ac849e7d38dfbd0
Graham Jackson <graham.jackson9@nhs.net>
2025-05-18 11:15:04 +0100

    DTOSS-9018: Refer to events rather than rows

--- projects/DTOSS-9018-SPIKE-Writing-from-our-laptop-to-ACTUAL-FDP-data-sets/main.py
+++ projects/DTOSS-9018-SPIKE-Writing-from-our-laptop-to-ACTUAL-FDP-data-sets/main.py
@@ -1,6 +1,8 @@
+import json
 import logging
 import os
 from pathlib import Path
+from typing import List
 
 from dotenv import load_dotenv
 import foundry_sdk
@@ -17,18 +19,32 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def create_dummy_json_lines_file(filename: str = "dummy", n_rows: int = 1_000, path=Path('/tmp/')) -> str:
+def create_list_of_events(n_events: int = 1_000, n_kv_pairs_per_event: int = 100) -> dict:
+    events = []
 
-    filename_prefix, filename_suffix = filename.split(".")
-    filepath = path / f"{filename_prefix}--{n_rows}-lines.{filename_suffix}"
+    for i in range(n_events):
+        payload = [{f"key_{j}": f"value_{j}"} for j in range(n_kv_pairs_per_event)]
+        events.append({"id": i, "name": "foo", "payload": payload})
+
+    logger.info(f"Created {n_events:,} events, each with {n_kv_pairs_per_event:,} key value pairs.")
+
+    return events
+
+
+def get_local_file_name(filename_description: str, n_events: int, n_kv_pairs_per_event: int) -> str:
+    return f"{filename_description}--{n_events}-events-by-{n_kv_pairs_per_event}-kv-pairs.jsonl"
+
+
+def create_json_lines_file(filename: str, events: List[dict], path=Path('/tmp/')) -> str:
+
+    filepath = path / filename
 
     with open(filepath, "w") as f:
-        for i in range(n_rows + 1):
-            f.write(f'{{"id": {i}, "name": "Name {i}"}}\n')
+        f.write("\n".join(map(json.dumps, events)))
 
     filesize_mb = os.path.getsize(filepath) / (1024 * 1024)
 
-    logger.info(f"Created dummy file with {n_rows:,} JSON lines: {filepath}, {filesize_mb:.2f} MB")
+    logger.info(f"Wrote JSON lines file: {filepath}, {filesize_mb:.2f} MB")
 
     return str(filepath)
 
@@ -55,9 +71,14 @@ if __name__ == "__main__":
     host: str = os.environ["HOSTNAME"]
     dataset_rid: str = os.environ["DATASET_RID"]
 
-    filename_local: str = 'dummy.jsonl'
-    filename_foundry: str = 'dummy_fdp.jsonl'
+    n_events: int = 3
+    n_kv_pairs_per_event: int = 2
 
-    filepath_local = create_dummy_json_lines_file(filename_local)
+    filename_description: str = 'dummy'
+
+    events = create_list_of_events(n_events, n_kv_pairs_per_event)
+    filename_local = get_local_file_name(filename_description, n_events, n_kv_pairs_per_event)
+
+    filepath_local = create_json_lines_file(filename_local, events)
     client = get_foundry_client(host, token)
-    upload_file_to_foundry_dataset(client, dataset_rid, filepath_local, filename_foundry)
+    upload_file_to_foundry_dataset(client, dataset_rid, filepath_local, filename_foundry=filename_local)
