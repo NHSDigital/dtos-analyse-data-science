c2da3c1e450c2ba68f45208c4957cca747e66650
Graham Jackson <graham.jackson9@nhs.net>
2025-05-18 17:38:09 +0100

    DTOSS-9018: Create datasets as needed

--- projects/DTOSS-9018-SPIKE-Writing-from-our-laptop-to-ACTUAL-FDP-data-sets/upload_file_to_foundry_dataset.py
+++ projects/DTOSS-9018-SPIKE-Writing-from-our-laptop-to-ACTUAL-FDP-data-sets/upload_file_to_foundry_dataset.py
@@ -1,7 +1,9 @@
 import argparse
+from datetime import datetime
 import logging
 import os
 import time
+import uuid
 
 from dotenv import load_dotenv
 import foundry_sdk
@@ -16,6 +18,9 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def get_foundry_dataset_name() -> str:
+    return f"raw-nsp-events--{datetime.now().strftime('%Y-%m-%d--%H-%M-%S')}--{uuid.uuid4()}"
+
 
 def get_foundry_client(host: str, token: str) -> FoundryClient:
     return foundry_sdk.FoundryClient(
@@ -24,7 +29,7 @@ def get_foundry_client(host: str, token: str) -> FoundryClient:
     )
 
 
-def upload_file_to_foundry_dataset(client: FoundryClient, dataset_rid: str, filepath_local: str, filename_foundry: str) -> None:
+def upload_file_to_foundry_dataset(client: FoundryClient, dataset_rid: str, filepath_local: str, foundry_filename: str) -> None:
     with open(filepath_local, "rb") as f:
         bytes_ = f.read()
 
@@ -34,7 +39,7 @@ def upload_file_to_foundry_dataset(client: FoundryClient, dataset_rid: str, file
 
     client.datasets.Dataset.File.upload(
         dataset_rid=dataset_rid,
-        file_path=filename_foundry,
+        file_path=foundry_filename,
         body=bytes_
     )
 
@@ -42,22 +47,26 @@ def upload_file_to_foundry_dataset(client: FoundryClient, dataset_rid: str, file
 
     upload_time_seconds = end_time - start_time
 
-    logger.info(f"File {filepath_local} uploaded to foundry as {filename_foundry} in {upload_time_seconds:.0f} seconds")
+    logger.info(f"File {filepath_local} uploaded to foundry as {foundry_filename} in {upload_time_seconds:.0f} seconds")
 
 
 if __name__ == "__main__":
     token: str = os.environ["BEARER_TOKEN"]
     host: str = os.environ["HOSTNAME"]
-    dataset_rid: str = os.environ["DATASET_RID"]
+    parent_folder_rid: str = os.environ["PARENT_FOLDER_RID"]
 
     parser = argparse.ArgumentParser(description="Upload a file to a Foundry dataset.")
-    parser.add_argument("filepath_local", type=str, help="Path to the local file to upload.")
-    parser.add_argument("--filename_foundry", type=str, default=None, help="Foundry destination file path.")
+    parser.add_argument("--filepath_local", type=str, default='/tmp/dummy.jsonl', help="Path to the local file to upload.")
+    parser.add_argument("--foundry_dataset_name", type=str, default=get_foundry_dataset_name(), help="Name of the dataset in Foundry.")
+    parser.add_argument("--foundry_filename", type=str, default=None, help="Foundry destination file path.")
     args = parser.parse_args()
 
     filepath_local: str = args.filepath_local
     filename_local: str = os.path.basename(filepath_local)
-    filename_foundry: str = args.filename_foundry or filename_local
+
+    foundry_dataset_name: str = args.foundry_dataset_name
+    foundry_filename: str = args.foundry_filename or filename_local
 
     client = get_foundry_client(host, token)
-    upload_file_to_foundry_dataset(client, dataset_rid, filepath_local, filename_foundry=filename_local)
+    result = client.datasets.Dataset.create(name=foundry_dataset_name, parent_folder_rid=parent_folder_rid)
+    upload_file_to_foundry_dataset(client, result.rid, filepath_local, foundry_filename=foundry_filename)
